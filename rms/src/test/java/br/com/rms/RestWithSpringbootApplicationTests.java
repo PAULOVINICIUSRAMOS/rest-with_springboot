ceb1b50caa239a91649e48ee5b94fbac64f6f75a
PAULOVINICIUSRAMOS <contato.pramos@gmail.com>
2023-09-22 14:22:12 -0300

    Initial commit

---
+++ rms/src/test/java/br/com/rms/RestWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
